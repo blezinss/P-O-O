abfb7dca612fbef00f596d2f13aab2d46778aa08
blezinss <104841658+blezinss@users.noreply.github.com>
2022-11-07 10:46:07 -0300

    Create delLivro.php

---
+++ delLivro.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Deletar </title>
+        <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
+    </head>
+
+    <body>
+        <div class="container">
+
+        <?php 
+
+            include_once("../servidor.php");
+
+                /* Deleta o produto */
+                $sql = "DELETE FROM `tb_livro` WHERE cod_liv = " . $_GET["cod_liv"];
+
+                /* Troca pra forma procedural */
+                $resp = $POO->query($sql);
+
+        ?>
+    </body>
+
+    <script src="../js/jquery-3.5.1.slim.min.js"></script>
+    <script src="../js/popper.min.js"></script>
+</html>
